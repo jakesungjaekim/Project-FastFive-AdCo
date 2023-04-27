fc8717b18aef251d96f1be903c645f4b65f165e9
jakesungjaekim <codejakecarry@gmail.com>
2023-04-27 18:34:47 +0900

    Add: BoilerPlate

---
+++ README copy.md
@@ -0,0 +1,3 @@
+# 설치 라이브러리
+
+Yarn, Vite , React, axios, react-router-dom, tailwindcss
---
+++ src/pages/Auth/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AuthPage from './components/AuthPage'
+
+const index = () => {
+  return (
+      <AuthPage />
+  )
+}
+
+export default index
